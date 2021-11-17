0743f9a7d99f88a11a4137dc5b05de4b61087f26
Patrick <patrick.schaefler@web.de>
2021-11-17 21:49:33 +0100

    added test function

--- main.go
+++ main.go
@@ -1,29 +1,16 @@
 package main
 
-import hmm "hidden-markov-model/src"
+import (
+	"hidden-markov-model/hmm"
+	"path"
+)
 
 func main() {
-	inputFile := "resources/models2.json"
-	markovModels := hmm.HiddenMarkovModels{}
-
-	err := markovModels.ReadModelsFromFile(inputFile)
-	if err != nil {
-		panic(err.Error())
+	testfiles := [3]string{"models1.json", "models2.json", "models3.json"}
+	for _, file := range testfiles {
+		err := hmm.TestHMMs(path.Join("resources", file))
+		if err != nil {
+			panic(err.Error())
+		}
 	}
-
-	err = markovModels.ReadObservationsFromFile(inputFile)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	markovModels.EvaluateModels()
 }
-
-/*
- Das Einlesen der Definition von (beliebig vielen) HMMs soll über Files möglich sein.
- Es soll ebenfalls über Files möglich sein, Sequenzen von Beobachtungen einzulesen.
- Der Forward Algorithmus soll verwendet werden, um zu bestimmen, welches der
-eingegebenen HMMs am ehesten zu den gegebenen Beobachtungen passt. Die Wahrscheinlichkeiten,
-mit welcher die HMMs und die Beobachtungen „zusammenpassen“, sollen ebenso ausgegeben werden
-(sowohl als Wahrscheinlichkeit als auch als log-likelihood).
-*/
